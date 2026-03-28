d11d1001b7257ce5a1e7b977d4fb1e9b0d3a8fc7
虹兵 牛 <2438012195@qq.com>
2026-03-28 16:45:00 +0800

    修改项目结构（未测试）

--- video-video-server/src/main/java/com/nhb/mapper/VideoMapper.java
+++
@@ -1,9 +0,0 @@
-package com.nhb.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.nhb.model.entity.Video;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface VideoMapper extends BaseMapper<Video> {
-}
--- video-video-server/src/main/java/com/nhb/service/VideoRecommendationsService.java
+++
@@ -1,9 +0,0 @@
-package com.nhb.service;
-
-import com.nhb.model.vo.RandomVideoInfoVO;
-
-import java.util.List;
-
-public interface VideoRecommendationsService {
-    List<RandomVideoInfoVO> getRandomVideoInfo(String num);
-}
